f674993bcf70055d84ed8750758206b948b9181a
ashish <officialwork.ahsish@gmail.com>
2022-09-22 20:36:00 +0530

    SOME FILES EDITED

--- MEMORY ADDRESS/memoryaddress.cpp
+++ MEMORY ADDRESS/memoryaddress.cpp
@@ -2,10 +2,12 @@
 #include <cmath>
 #include <string>
 
+using std::endl;
+using std::cout ;
+
 int main() {
 
-    std::string name = "ASHISH";
-    std::cout<< &name << std::endl;
-    return 0;
-
+  std::string name = "ASHISH";
+  std::cout << &name << endl;
+  return 0;
 }
