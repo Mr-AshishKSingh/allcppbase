333d22586b21695ab0093f0e287cc0e023b4d14b
ashish <officialwork.ahsish@gmail.com>
2022-09-22 22:56:56 +0530

    NEW FOLDERS ADDED

---
+++ cin/cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+string name ;
+    cout << "WRITE YOUR NAME HERE  " << endl;
+    cin >> name ;
+    return 0;
+
+
+}
